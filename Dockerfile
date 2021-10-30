a25e740c8570e9918369a8432dff8563752f836c
George <mazzeogeorge@gmail.com>
2021-10-29 20:40:04 -0400

    connected postgress

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,8 @@ FROM python:3.9.4-slim-buster
 WORKDIR /app
 
 COPY requirements.txt requirements.txt
+RUN apt-get update
+RUN apt-get -y install libpq-dev gcc
 RUN pip3 install -r requirements.txt
 
 COPY . .
--- config.py
+++ config.py
@@ -3,11 +3,14 @@ import os
 
 def get_sqlite_uri():
     basedir = os.path.abspath(os.path.dirname(__file__))
-    db_name = os.environ['DATABASE_URL'].split('/')[-1]
+    db_name = os.environ['SQLITE_DATABASE_URL'].split('/')[-1]
     return f'sqlite:///{basedir}/{db_name}'
 
+def get_postgress_uri():
+    return os.environ['HEROKU_POSTGRESQL_RED_URL']
 
 class BaseConfig:
-    SQLALCHEMY_DATABASE_URI = get_sqlite_uri()
+    # get_sqlite_uri()
+    SQLALCHEMY_DATABASE_URI = get_postgress_uri()
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ['SECRET_KEY']

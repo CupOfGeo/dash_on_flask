af67c6d140493713839eabef2d7e573e33a773bf
George <mazzeogeorge@gmail.com>
2021-10-30 00:39:12 -0400

    making more issues than solving

--- app/webapp2.py
+++ app/webapp2.py
@@ -7,6 +7,7 @@ from flask_login import current_user
 from flask_login import login_required
 from flask_login import login_user
 from flask_login import logout_user
+from oauthlib.oauth2 import TokenExpiredError
 from werkzeug.urls import url_parse
 
 from app.extensions import db
@@ -23,10 +24,14 @@ google_bp = make_google_blueprint(scope=["profile", "email"])
 
 @google_bp.route("/")
 def index():
+
     if not google.authorized:
         return redirect(url_for("google.login"))
-    resp = google.get("/oauth2/v1/userinfo")
-    assert resp.ok, resp.text
+    try:
+        resp = google.get("/oauth2/v1/userinfo")
+        assert resp.ok, resp.text
+    except TokenExpiredError as e:
+        return redirect(url_for("google.login"))
     return "You are {email} on Google".format(email=resp.json()["email"])
 
 @google_bp.route('/register/', methods=['GET', 'POST'])

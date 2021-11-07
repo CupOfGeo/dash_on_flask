aebb9bfa522b08e98182b82c06750a8e3ef7d88a
George <mazzeogeorge@gmail.com>
2021-11-07 18:20:57 -0500

    google app key

--- dashapp.py
+++ dashapp.py
@@ -1,3 +1,7 @@
 from app import create_app
-
+import os
+key = os.environ['GOOGLE_CRED']
+with open('google_key.json') as file:
+    file.write(key)
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'google_key.json'
 server = create_app()

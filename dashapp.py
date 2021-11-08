22b8ba5ba06c27688985aed05c22bd0c667154da
George <mazzeogeorge@gmail.com>
2021-11-08 00:11:11 -0500

    i pushed my key bc im dumb

--- dashapp.py
+++ dashapp.py
@@ -4,4 +4,5 @@ key = os.environ['GOOGLE_CRED']
 with open('google_key.json', 'w+') as file:
     file.write(key)
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'google_key.json'
+
 server = create_app()
